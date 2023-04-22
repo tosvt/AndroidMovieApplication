4169d42597ceac86a9402a3f7913f185fe4c19cc
tosvt <sweb7@ya.ru>
2023-04-22 19:21:39 +0300

    added datastatus

---
+++ app/src/main/java/com/example/mymovieapplication/utils/DataStatus.kt
@@ -0,0 +1,23 @@
+package com.example.mymovieapplication.utils
+
+data class DataStatus<out T> (
+    val status : Status,
+    val data : T?=null,
+    val message : String?= null,
+    val isEmpty : Boolean?=false
+) {
+    enum class Status {
+        LOADING , SUCCESS , ERROR
+    }
+    companion object{
+        fun <T> loading() : DataStatus<T>{
+            return DataStatus(Status.LOADING)
+        }
+        fun <T> success(data : T? , isEmpty: Boolean?) : DataStatus<T>{
+            return DataStatus(Status.SUCCESS, data, isEmpty=isEmpty)
+        }
+        fun <T> error(error : String): DataStatus<T> {
+            return DataStatus(Status.ERROR, message = error)
+        }
+    }
+}
